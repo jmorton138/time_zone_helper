7ca17e465a4dd62547b8b689db307be9344ea6ee
Jeff Morton <jmorton138@gmail.com>
2023-11-11 07:12:55 +0000

    [ADD] Adds functionality for converting time at one TZ to time in another

--- app/src/main/java/com/authenticator/timezonehelper/MainViewModel.kt
+++ app/src/main/java/com/authenticator/timezonehelper/MainViewModel.kt
@@ -1,9 +1,11 @@
 package com.authenticator.timezonehelper
 
+import android.icu.text.SimpleDateFormat
 import androidx.lifecycle.*
 import us.dustinj.timezonemap.TimeZone
 import us.dustinj.timezonemap.TimeZoneMap
 import java.time.*
+import java.util.*
 
 class MainViewModel(private val cityRepository: CityRepository, private val timeZoneMap: TimeZoneMap): ViewModel() {
     suspend fun getCityNames(): List<String> = cityRepository.getAllCityNames()
@@ -19,10 +21,28 @@ class MainViewModel(private val cityRepository: CityRepository, private val time
         return timeZone?.zoneId ?: "Not Found"
     }
 
-    fun getTimezoneCurrentTime(timeZone: String) : Long {
-        val zoneId: ZoneId = ZoneId.of("America/New_York")
-        val currentZonedDateTime: ZonedDateTime = ZonedDateTime.now(zoneId)
-        val currentTimestamp: Long = currentZonedDateTime.toInstant().toEpochMilli()
-        return currentTimestamp
+    fun getTimezoneCurrentTime(timeZone: String): LocalDateTime {
+        val zoneId: ZoneId = ZoneId.of(timeZone)
+        return LocalDateTime.now(zoneId)
+    }
+
+    fun convertToDestinationTime(timeZoneSource: CharSequence, timeZoneDest: CharSequence, sourceTime: CharSequence): String {
+        // TODO: refactor with time formatting helper method
+        val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault())
+
+        // Parse source time string into Date object
+        val formattedSourceDateTime = simpleDateFormat.parse(sourceTime.toString())
+
+        // Get Local times at source and destination
+        val sourceLocalTime = getTimezoneCurrentTime(timeZoneSource.toString())
+        val destLocalTime = getTimezoneCurrentTime(timeZoneDest.toString())
+
+        val timeDiff = Duration.between(sourceLocalTime, destLocalTime)
+
+        // Add time difference to destination time and format for rendering as a string in the UI
+        val calendar = Calendar.getInstance()
+        calendar.time = formattedSourceDateTime
+        calendar.add(Calendar.SECOND, timeDiff.seconds.toInt())
+        return calendar.time.toString()
     }
 }
